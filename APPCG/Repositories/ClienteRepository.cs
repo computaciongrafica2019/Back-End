253f38811a7a13fd678d7ae4b284d135b8759b8e
Heyner <hsmartineza@unal.edu.co>
2019-07-05 11:05:39 -0500

    Arquitectura Base

---
+++ APPCG/Repositories/ClienteRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using APPCG.Models;
+
+
+namespace APPCG.Repositories
+{
+    public class ClienteRepository
+    {
+
+
+        public Cliente GetCliente(int idCliente)
+        {
+
+            Cliente cliente = null;
+
+            try
+            {
+
+                using (var db = new CG2019Entities())
+                {
+
+                    cliente = db.Cliente.Where(cli => cli.idCliente == idCliente).FirstOrDefault();
+
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+
+            return cliente;
+
+        }
+
+
+        public void CreateCliente(Cliente cliente)
+        {
+
+            try
+            {
+
+                using (var db = new CG2019Entities())
+                {
+
+                   db.Cliente.Add(cliente);
+                            
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+
+
+    }
+}
